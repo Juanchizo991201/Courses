075617d62f77dd32a8e6b05e28da97a34f0e3519
juanchizo991201 <juan.montenegro@correo.usa.edu.co>
2021-06-09 13:27:09 -0500

    avance ejercicio db.py

--- python/Diccionarios/db.py
+++ python/Diccionarios/db.py
@@ -16,30 +16,49 @@ while option != "":
         os.system("clear")
         print("Añadir cliente\n\n")
 
-        nif = int(input("Digite el numero unico de identificación:\n\n"))
-        os.system("clear")
-        nombre = str(input("Digite el nombre:\n\n")).capitalize()
-        os.system("clear")
-        direccion = str(input("Digite la dirección:\n\n"))
-        os.system("clear")
-        telefono = str(input("Digite el telefono:\n\n"))
-        os.system("clear")
-        correo = str(input("Digite el correo:\n\n"))
-        os.system("clear")
-        Preferente = int(
-            input("Digite 1 si es preferente, 0 si no lo es:\n\n"))
-        os.system("clear")
+        try:
+            nif = int(input("Digite el numero unico de identificación:\n\n"))
+            os.system("clear")
+        except:
+            os.system("clear")
+            print("Numero de identifiación invalido")
+            nif = 0
+            time.sleep(2)
+        
+        if nif != 0:
+            nombre = str(input("Digite el nombre:\n\n")).capitalize()
+            os.system("clear")
+            direccion = str(input("Digite la dirección:\n\n"))
+            os.system("clear")
+            telefono = str(input("Digite el telefono:\n\n"))
+            os.system("clear")
+            correo = str(input("Digite el correo:\n\n"))
+            os.system("clear")
+            preferente = int(input("Digite 1 si es preferente, o 0 si no lo es:\n\n"))
+            os.system("clear")
+            try:
+                if int(preferente) == 0 or int(preferente) == 1:
+                    print()
+                
+            except:
+                preferente = 10
+                print("valor de preferencia invalido, vuelva a añadir al cliente")
+                time.sleep(3)
 
-        infoDictionary["nombre"] = nombre
-        infoDictionary["direccion"] = direccion
-        infoDictionary["telefono"] = telefono
-        infoDictionary["correo"] = correo
-        infoDictionary["preferente"] = Preferente
+            if preferente < 2:
+                infoDictionary["nombre"] = nombre
+                infoDictionary["direccion"] = direccion
+                infoDictionary["telefono"] = telefono
+                infoDictionary["correo"] = correo
+                infoDictionary["preferente"] = preferente
 
-        personalInfoDicionary[nif] = infoDictionary
+                personalInfoDicionary[nif] = infoDictionary
 
-        print("Añadiendo...")
-        time.sleep(1)
+                print("Añadiendo...")
+                time.sleep(1)
+            else:
+                print("valor de preferencia invalido, vuelva a añadir al cliente")
+                time.sleep(1)
 
     elif option == "2":
         os.system("clear")
@@ -92,7 +111,7 @@ while option != "":
 
         else:
             print("No hay clientes para ejecutar esta operación")
-            time.sleep(1)
+            time.sleep(2)
 
     elif option == "4":
         os.system("clear")
@@ -100,11 +119,15 @@ while option != "":
 
         if len(personalInfoDicionary) > 0:
 
-            keys = personalInfoDicionary.keys()
+            keys = list(personalInfoDicionary.keys())
+            print(keys)
+            time.sleep(2)
 
-            for i in range(len(keys)):
-                infoDictionary = personalInfoDicionary[str(keys[i])]
-                print(infoDictionary)
+            # for i in range(len(keys)):
+            #     infoDictionary = personalInfoDicionary[int(keys.pop)]
+            #     print(infoDictionary)
+            #     time.sleep(1)
+            
         else:
             print("No hay clientes para ejecutar esta operación")
             time.sleep(1)
