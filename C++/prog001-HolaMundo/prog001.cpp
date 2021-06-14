d1270cca77b31b0f549ed9155d6c0c1b7952be86
juanchizo991201 <juan.montenegro@correo.usa.edu.co>
2021-06-14 13:49:23 -0500

    c++ added

---
+++ C++/prog001-HolaMundo/prog001.cpp
@@ -0,0 +1,18 @@
+/*********************************************************************************/
+/*                                                                               */
+/*                          Mi primer programa en c++ 💻                         */
+/*                               14 - junio - 2021                               */
+/*                                                                               */
+/*********************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    cout << "\n\nHello World,\nWelcome to my first C++ program on Ubuntu Linux\n\n"
+         << endl;
+    return 0;
+}
